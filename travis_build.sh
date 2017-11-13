25ecf06f8c0b71a7cc5dd053f3583046987a0e7c
Andrew Chiw <randomshinichi4869@gmail.com>
2017-11-13 18:47:44 +0100

    Travis Stage Refactor

--- travis_build.sh
+++ travis_build.sh
@@ -5,7 +5,7 @@ USER_INFO="$( id -u ${USER} ):$( id -g ${USER} )"
 SHARE_USER_INFO="-v /etc/group:/etc/group:ro -v /etc/passwd:/etc/passwd:ro"
 SHARE_SRC="-v $(pwd):/travis"
 
-docker stop $(docker ps -aq --filter name=builder) || true
-docker rm $(docker ps -aq --filter name=builder) || true
+docker stop $(docker ps -aq --filter name=worker-${PLATFORM}) || true
+docker rm $(docker ps -aq --filter name=worker-${PLATFORM}) || true
 docker build --file Dockerfile.${PLATFORM} -t builder-${PLATFORM} .
-docker run --name builder -t -e USER_INFO=${USER_INFO} -e PROTOBUF_VER=${PROTOBUF_VER} -e GRPCIO_VER=${GRPCIO_VER} ${SHARE_SRC} ${SHARE_USER_INFO} builder-${PLATFORM} /build.sh
+docker run --name worker-${PLATFORM} -t -e USER_INFO=${USER_INFO} -e PROTOBUF_VER=${PROTOBUF_VER} -e GRPCIO_VER=${GRPCIO_VER} ${SHARE_SRC} ${SHARE_USER_INFO} builder-${PLATFORM} /build.sh
