fd42293ff449b0fd7d41120818af8623ec5f5465
Andrew Chiw <randomshinichi4869@gmail.com>
2017-11-13 18:08:58 +0100

    Double quotes did not work in build script

--- build.sh
+++ build.sh
@@ -10,5 +10,5 @@ SHARE_SRC="-v $(pwd):/travis"
 docker build --file Dockerfile.debian-stretch-deps -t stretch-deps .
 docker build --file Dockerfile.ubuntu-xenial-deps -t xenial-deps .
 
-docker run -t -e USER_INFO=${USER_INFO} -e PROTOBUF_VER=${PROTOBUF_VER} -e GRPCIO_VER=${GRPCIO_VER} ${SHARE_SRC} "${SHARE_USER_INFO}" stretch-deps /build.sh
-docker run -t -e USER_INFO=${USER_INFO} -e PROTOBUF_VER=${PROTOBUF_VER} -e GRPCIO_VER=${GRPCIO_VER} ${SHARE_SRC} "${SHARE_USER_INFO}" xenial-deps /build.sh
+docker run -t -e USER_INFO=${USER_INFO} -e PROTOBUF_VER=${PROTOBUF_VER} -e GRPCIO_VER=${GRPCIO_VER} ${SHARE_SRC} ${SHARE_USER_INFO} stretch-deps /build.sh
+docker run -t -e USER_INFO=${USER_INFO} -e PROTOBUF_VER=${PROTOBUF_VER} -e GRPCIO_VER=${GRPCIO_VER} ${SHARE_SRC} ${SHARE_USER_INFO} xenial-deps /build.sh
