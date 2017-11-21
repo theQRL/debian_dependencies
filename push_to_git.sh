aa5d0e4e1fa6b864d54ab3cd9cf04cca687886df
Andrew Chiw <randomshinichi4869@gmail.com>
2017-11-21 14:07:48 +0000

    .travis.yml: don't need to repeat some env variables so much anymore stretch: same buildscript as xenial

--- push_to_git.sh
+++ push_to_git.sh
@@ -1,6 +1,6 @@
 git checkout --orphan ${PLATFORM}
 git rm --cached -r .
-git add ${BUILD_DIR_PLATFORM}
+git add built/${PLATFORM}
 git commit -m "Protobuf and gRPC binaries"
 git push https://randomshinichi:$GITHUB_TOKEN@github.com/randomshinichi/qrllib-deps-builder.git HEAD:${PLATFORM} -f
 
--- stretch/build_dummy.sh
+++ stretch/build_dummy.sh
@@ -2,4 +2,6 @@
 gpg --import /secrets/public.gpg
 gpg --import /secrets/private.gpg
 
+mkdir -p /travis/${PLATFORM}
 touch /travis/${PLATFORM}/build_results
+chown -R $USER_INFO /travis
