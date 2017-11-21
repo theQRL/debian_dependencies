3f09a6ac62bdbd084d81a7c196f69401f88fa0c4
Andrew Chiw <randomshinichi4869@gmail.com>
2017-11-21 18:25:24 +0000

    revised checking if branch already exists

--- push_to_git.sh
+++ push_to_git.sh
@@ -1,13 +1,13 @@
 set -e
 echo "PUSHING TO GITHUB PAGES"
-if [ `git branch --list ${PLATFORM}` ]
+if [ `git rev-parse --quiet --verify ${PLATFORM}` > /dev/null ]
 then
     echo "Branch ${PLATFORM} already exists, deleting"
     git branch -D ${PLATFORM}
 fi
 
 git checkout --orphan ${PLATFORM}
-git rm --cached -r .
+git rm -rf .
 git add built/${PLATFORM}
 git commit -m "Protobuf and gRPC binaries"
 git push https://randomshinichi:$GITHUB_TOKEN@github.com/randomshinichi/qrllib-deps-builder.git HEAD:${PLATFORM} -f
