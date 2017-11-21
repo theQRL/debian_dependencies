1348b722beac4894254405d47438b17b08e94d5a
Andrew Chiw <randomshinichi4869@gmail.com>
2017-11-21 18:42:04 +0000

    Updated merge_git.sh to work with what's going on now

--- merge_git.sh
+++ merge_git.sh
@@ -7,16 +7,16 @@ cd working_dir/qrllib-deps-builder
 git remote -v
 git branch -a
 
-git checkout --track origin/ubuntu-xenial
-cp -r built/* ..
+git checkout --track origin/xenial
+cp -r built/xenial ..
 
-git checkout --track origin/debian-stretch
-cp -r built/* ..
+git checkout --track origin/stretch
+cp -r built/stretch ..
 
 git checkout --track origin/gh-pages
 rm -rf * .gitignore .travis.yml
-cp -r ../ubuntu-xenial ../debian-stretch .
+cp -r ../xenial ../stretch .
 
 git add .
-git commit -m "Protobuf ${PROTOBUF_VER}, gRPC ${GRPCIO_VER}"
+git commit -m "Protobuf, gRPC binaries, signed and all supported distros"
 git push https://randomshinichi:$GITHUB_TOKEN@github.com/randomshinichi/qrllib-deps-builder.git HEAD:gh-pages -f
